1fbcf8aace22a2ec3816bade2027bd64f47eb271
Andres-Ayala1 <andresayala@utexas.edu>
2022-05-07 19:20:00 -0500

    made app/in need of edits

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,19 @@
+function retrieveCompl(){
+    fetch('https://api.adviceslip.com/advice')
+        .then(response=>
+         {
+            if(!response.ok)
+                throw new Error('HTTP error: ${response.status}');
+            return response.text();
+        }).then(text => updateComplDisplay(JSON.parse(text)));
+}
+function updateComplDisplay(slipObj){
+    var complObj = slipObj['slip'];
+    document.querySelector("#advice-id").textContent = id;
+    var {id, advice} = complObj;
+    document.querySelector(".advice-text").textContent = '"'+advice+'"';
+}   
+//document.querySelector('.dice-container')
+    //.addEventListener('click', retrieveCompl);
+
+//retrieveCompl();
